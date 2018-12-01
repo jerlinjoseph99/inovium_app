cb8fb6fdfee9dac7ec45ecaab2fe36080fbe5097
jerlin joseph <jerlin.urbanecho@gmail.com>
2018-12-01 23:17:54 +0000

    Added Bootstrap and theme styling to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/science'
+  
+  root 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
